34c744ac49775d5c7db0f6d8d8cba0a93391bb4d
Alex <abdiep@ualberta.ca>
2024-03-27 11:18:45 -0600

    asd

--- A4/matplotlib/plot.py
+++ A4/matplotlib/plot.py
@@ -3,51 +3,67 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Read data from csv file
-df = pd.read_csv('Lab 1_ Dimensional Measurement - Q3 Plotting Data.csv')
+df = pd.read_excel(r"A4\analysis.xlsx", sheet_name="recirc combined")
 
-# Remove the 'STDEV', 'T-INV (or Z-Inv)' 'Accuracy' columns
-df = df.drop(['STDEV', 'T-INV (or Z-Inv)', 'Accuracy'], axis=1)
+grid1 = df[df["Grid Number"] == 1]
+grid2 = df[df["Grid Number"] == 2]
+grid3 = df[df["Grid Number"] == 3]
+grid4 = df[df["Grid Number"] == 4]
+grid5 = df[df["Grid Number"] == 5]
 
-# Sort 'device' == 'Micrometer'
-df_micrometer = df[df['Device'] == 'Micrometer']
-df_vernier = df[df['Device'] == 'Vernier Caliper']
-df_digital = df[df['Device'] == 'Digital Caliper']
-
-# Plot the data
-plt.plot(df_micrometer['n'], df_micrometer['P'], '-o', label='Micrometer', markersize=6, markerfacecolor='none')
-plt.plot(df_vernier['n'], df_vernier['P'], '-s', label='Vernier Caliper', markersize=6, markerfacecolor='none')
-plt.plot(df_digital['n'], df_digital['P'], '-^', label='Digital Caliper', markersize=6, markerfacecolor='none')
-
-
-# Set the title and labels
-plt.xlabel(r'$n$, Number of Measurements')
-plt.ylabel(r'$P_{x}$, Precision (mm)')
-plt.legend()
-
-# Set x-axis ticks to integer values with tick mark rotation
-plt.xticks(np.arange(df['n'].min(), df['n'].max()+1, 1), rotation=45, ha = 'left')
-
-# Save the plots
-plt.savefig('Q3_Precision.png', dpi=300)
-
-# Make new plot
+# Plot all grids on the same plot
 plt.figure()
-plt.plot(df_micrometer['n'], df_micrometer['U'], '-o', label='Micrometer', markersize=6, markerfacecolor='none')
-plt.plot(df_vernier['n'], df_vernier['U'], '-s', label='Vernier Caliper', markersize=6, markerfacecolor='none')
-plt.plot(df_digital['n'], df_digital['U'], '-^', label='Digital Caliper', markersize=6, markerfacecolor='none')
+plt.plot(grid1["x"], grid1["y"], "-", label="Grid 1")
+plt.plot(grid2["x"], grid2["y"], "--", label="Grid 2")
+plt.plot(grid3["x"], grid3["y"], "-.", label="Grid 3")
+plt.plot(grid4["x"], grid4["y"], "-", label="Grid 4")
+plt.plot(grid5["x"], grid5["y"], "--", label="Grid 5")
 
 # Set the title and labels
-plt.xlabel(r'$n$, Number of Measurements')
-plt.ylabel(r'$U_{x}$, Uncertainty (mm)')
+plt.xlabel(r"$x$ (m)")
+plt.ylabel(r"$u$ (m/s)")
 plt.legend()
 
-# Set x-axis ticks to integer values with tick mark rotation
-plt.xticks(np.arange(df['n'].min(), df['n'].max()+1, 1), rotation=45, ha = 'left')
+# Save the plots
+plt.savefig(r"A4\Questions\Figures\recirc_combined.png", dpi=300)
+
+# Read more data
+df = pd.read_excel(r"A4\analysis.xlsx", sheet_name="per cells")
+
+# Plot recirculation vs cell count
+plt.figure()
+plt.plot(
+    df["cells"],
+    df["length"],
+    "-o",
+    label="Recirculation Length",
+    markersize=6,
+    markerfacecolor="none",
+)
+
+# Set the title and labels
+plt.xlabel(r"Number of Cells")
+plt.ylabel(r"$L_r$ Recirculation Length (m)")
+plt.legend()
 
 # Save the plots
-plt.savefig('Q3_Uncertainty.png', dpi=300)
+plt.savefig(r"A4\Questions\Figures\recirc_length_vs_cells.png", dpi=300)
 
-# Show the plot
-plt.show()
+# Plot drag coefficient vs cell count
+plt.figure()
+plt.plot(
+    df["cells"],
+    df["drag"],
+    "-o",
+    label="Drag Coefficient",
+    markersize=6,
+    markerfacecolor="none",
+)
 
+# Set the title and labels
+plt.xlabel(r"Number of Cells")
+plt.ylabel(r"$C_d$ Drag Coefficient")
+plt.legend()
 
+# Save the plots
+plt.savefig(r"A4\Questions\Figures\drag_coefficient_vs_cells.png", dpi=300)
